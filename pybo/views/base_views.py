1e63f38d30b2757392190908a8b92551c884668e
박인영 <sand1155@naver.com>
2021-02-27 17:45:38 +0900

    파이보 완료

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,12 +4,15 @@ from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
     """
     pybo 목록 출력
     """
     # 입력 파라미터
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
     so = request.GET.get('so', 'recent')  # 정렬기준
